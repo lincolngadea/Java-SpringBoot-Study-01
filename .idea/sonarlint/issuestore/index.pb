ed29830ed4c6fcba2e109450ee653d605ed656e2
Lincoln Gadea <lincoln@glinc.com.br>
2021-02-13 22:07:25 -0300

    chore:criar um hello world

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+m
+=src/main/java/io/java/curso/springboot/VendasApplication.java,3\5\3567d02c4bbb7bf8d8d5ec6efc2416f427e3771f
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/main/java/io/java/curso/springboot/VendasApplication.java
@@ -0,0 +1,21 @@
+package io.java.curso.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String helloWord(){
+        return "Hello Word";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+
+
+    }
+}
