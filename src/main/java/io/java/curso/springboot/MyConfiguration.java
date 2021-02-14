72cc30fe9f5e9edf5f9bf2f72ee7aeccec222ad4
Lincoln Gadea <lincoln@glinc.com.br>
2021-02-14 00:32:11 -0300

    chore: cria profiles,ambientes de profiles e application.properties

--- src/main/java/io/java/curso/springboot/MyConfiguration.java
+++ src/main/java/io/java/curso/springboot/MyConfiguration.java
@@ -1,12 +1,17 @@
 package io.java.curso.springboot;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistema de Vendas";
+    @Bean
+   public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO NO AMBIENTE DE DESENVOLVIMENTO!");
+        };
     }
 }
