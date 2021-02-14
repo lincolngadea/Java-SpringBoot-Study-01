15e1b9bc77bcc8c381b9435f4b324950c692cb3c
Lincoln Gadea <lincoln@glinc.com.br>
2021-02-14 16:52:43 -0300

    chore: cria interface PedidoRepository e estende com JpaRepository

---
+++ src/main/java/io/java/curso/springboot/repository/PedidoRepository.java
@@ -0,0 +1,9 @@
+package io.java.curso.springboot.repository;
+
+import io.java.curso.springboot.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+}
---
+++ src/main/java/io/java/curso/springboot/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package io.java.curso.springboot.repository;
+
+import io.java.curso.springboot.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
