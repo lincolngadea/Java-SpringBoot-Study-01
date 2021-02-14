927f1024ac1546b5d4648853c2dacbe44fa2ede8
Lincoln Gadea <lincoln@glinc.com.br>
2021-02-13 23:37:40 -0300

    chore: cria exemplo de injeção de dependência no spring

---
+++ src/main/java/io/java/curso/springboot/repository/ClienteRepository.java
@@ -0,0 +1,12 @@
+package io.java.curso.springboot.repository;
+
+import io.java.curso.springboot.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClienteRepository {
+
+    public void persistir(Cliente cliente){
+        //Acessar a base e salva o cliente
+    }
+}
