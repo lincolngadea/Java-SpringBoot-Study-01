1f42668b871e2bbff5a2d3e6d9498d7884a5b1b1
Lincoln Gadea <lincoln@glinc.com.br>
2021-02-14 11:32:46 -0300

    chore: lista clientes via jdbctemplate;

--- src/main/java/io/java/curso/springboot/VendasApplication.java
+++ src/main/java/io/java/curso/springboot/VendasApplication.java
@@ -8,15 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class VendasApplication {
 
     @Bean
     public CommandLineRunner init(@Autowired ClienteRepository clienteRepository){
         return args -> {
-            Cliente cliente = new Cliente();
-            cliente.setNome("Douglas");
-            clienteRepository.salvar(cliente);
+           clienteRepository.salvar(new Cliente("Doublas"));
+           clienteRepository.salvar(new Cliente("Lincoln"));
+
+           List<Cliente> listaDeClientes = clienteRepository.listarClientes();
+           listaDeClientes.forEach(System.out::println);
         };
     }
 
--- src/main/java/io/java/curso/springboot/repository/ClienteRepository.java
+++ src/main/java/io/java/curso/springboot/repository/ClienteRepository.java
@@ -3,12 +3,18 @@ package io.java.curso.springboot.repository;
 import io.java.curso.springboot.domain.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class ClienteRepository {
 
     private static String INSERT = "insert into cliente (nome) values (?)";
+    private static String SELECT_ALL = "select * from cliente";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -16,5 +22,16 @@ public class ClienteRepository {
     public Cliente salvar(Cliente cliente){
         jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
         return cliente;
+    }
+
+    public List<Cliente> listarClientes(){
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+            @Override
+            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+                Integer id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                return new Cliente(id,nome);
+            }
+        });
     }
 }
