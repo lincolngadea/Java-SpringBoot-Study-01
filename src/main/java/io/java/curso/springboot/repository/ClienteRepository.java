f15ae9b2be503caf06046de74e0f8852aa4e25fb
Lincoln Gadea <lincoln@glinc.com.br>
2021-02-14 11:46:59 -0300

    chore:cria metodos atualizar e deletar

--- src/main/java/io/java/curso/springboot/repository/ClienteRepository.java
+++ src/main/java/io/java/curso/springboot/repository/ClienteRepository.java
@@ -15,23 +15,37 @@ public class ClienteRepository {
 
     private static String INSERT = "insert into cliente (nome) values (?)";
     private static String SELECT_ALL = "select * from cliente";
+    private static String UPDATE = "update cliente set nome = ? where id = ?";
+    private static String DELETE = "delete from cliente where id = ?";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public Cliente salvar(Cliente cliente){
+    public Cliente salvar(Cliente cliente) {
         jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
         return cliente;
     }
 
-    public List<Cliente> listarClientes(){
+    public List<Cliente> listarClientes() {
         return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
                 Integer id = resultSet.getInt("id");
                 String nome = resultSet.getString("nome");
-                return new Cliente(id,nome);
+                return new Cliente(id, nome);
             }
         });
+    }
+
+    public Cliente atualizar(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(),cliente.getId()});
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+    }
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE,new Object[]{id});
     }
 }
