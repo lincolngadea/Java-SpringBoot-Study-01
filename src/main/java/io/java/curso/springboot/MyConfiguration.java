76059af1f613b23724ee79030a8c3f3a155cc0bf
Lincoln Gadea <lincoln@glinc.com.br>
2021-02-14 00:43:15 -0300

    chore: cria exemplo de criação de uma anotation

--- src/main/java/io/java/curso/springboot/MyConfiguration.java
+++ src/main/java/io/java/curso/springboot/MyConfiguration.java
@@ -1,11 +1,9 @@
 package io.java.curso.springboot;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class MyConfiguration {
 
     @Bean
