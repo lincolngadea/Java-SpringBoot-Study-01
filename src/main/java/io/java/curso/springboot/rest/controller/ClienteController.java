7f28e3dcdd3905f599429ff6554b2de45bd69bda
Lincoln Gadea <lincoln@glinc.com.br>
2021-02-16 11:28:24 -0300

    refactor: optimiza o mapeamento para já trazer o método implícito

--- src/main/java/io/java/curso/springboot/rest/controller/ClienteController.java
+++ src/main/java/io/java/curso/springboot/rest/controller/ClienteController.java
@@ -1,16 +1,13 @@
 package io.java.curso.springboot.rest.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value= "/hello/{nome}", method = RequestMethod.GET)
+    @GetMapping(value= "/hello/{nome}")
     @ResponseBody
     public String helloCliente(@PathVariable("nome") String nomeCliente){
         return String.format("Hello %s ", nomeCliente);
